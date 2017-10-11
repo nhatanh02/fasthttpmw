8b20e95b0ce11c99fd50e8debc3f875d019aea08
nhatanh02 <anh.ngo@cis.net.vn>
2017-10-11 17:53:39 +0700

    Added example of translation from echo.Context.Bind() and         echo.Context.JSON() to fasthttp.RequestCtx's methods 	modified:   testapp.go

--- testapp.go
+++ testapp.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	//fastrouter "github.com/buaazp/fasthttprouter"
+	"bytes"
+	"encoding/json"
 	"fasthttp-mw/middlewares"
 	"fasthttp-mw/routerwithmw"
 	"github.com/valyala/fasthttp"
 	"log"
+	//"strings"
 )
 
 func main() {
@@ -27,20 +30,33 @@ func main() {
 			fmt.Fprintf(ctx, "%s", ctx.UserValue("a"))
 		}
 	}
+	postHandler := func(ctx *fasthttp.RequestCtx) {
+		var v *Resp = new(Resp)
+		var u Resp
+		ctx.Request.BodyWriteTo(v)
+		fmt.Println(v)
+		jsval, _ := json.Marshal(*v)
+		fmt.Println(jsval)
+		ctx.SetStatusCode(fasthttp.StatusOK)
+		ctx.SetBody(jsval)
+		//test unmarshalling
+		json.Unmarshal(jsval, &u)
+		fmt.Println(u)
+	}
 	router := routerwithmw.New()
 	router.Use(middlewares.Recover())
-	router.Use(middlewares.BodyLimit("1B"))
+	//router.Use(middlewares.BodyLimit("1B"))
 	router.Use(middlewares.BasicAuth(func(username string, password string, c *fasthttp.RequestCtx) (bool, error) {
 		if username == "joe" && password == "secret" {
 			return true, nil
 		}
 		return false, nil
 	}))
-	router.Use(middlewares.Secure())
-	router.Use(middlewares.JWT(""))
-	router.Use(middlewares.CORS())
-	router.POST("/*a", requestHandler)
-	//router.GET("/*a", requestHandler)
+	//router.Use(middlewares.Secure())
+	//router.Use(middlewares.JWT(""))
+	//router.Use(middlewares.CORS())
+	router.POST("/*a", postHandler)
+	router.GET("/*a", requestHandler)
 	//router.GET("/:a", requestHandler)
 	// Start the server with default settings.
 	// Create Server instance for adjusting server settings.
@@ -49,4 +65,38 @@ func main() {
 	if err := fasthttp.ListenAndServe(listenAddr, router.Handler); err != nil {
 		log.Fatalf("error in ListenAndServe: %s", err)
 	}
+}
+
+//write body to struct example
+
+type Resp struct {
+	A string `json:"a"`
+	B string `json:"b"`
+}
+
+func (r *Resp) Write(p []byte) (n int, err error) {
+	//lines(p), with some light trimming
+	fields := bytes.Split(bytes.Trim(p, " \n"), []byte("\n"))
+	for _, field := range fields {
+		//nil check
+		if bytes.Compare(field, nil) == 0 {
+			continue
+		}
+		//split each line to a k-v pair
+		kv := bytes.SplitN(field, []byte{':'}, 2)
+		if kv == nil {
+			continue
+		}
+		k := bytes.Trim(kv[0], " ")
+		v := bytes.Trim(kv[1], " ")
+		//writing to struct
+		switch string(bytes.ToLower(k)) {
+		case "a":
+			r.A = string(v)
+		case "b":
+			r.B = string(v)
+		}
+	}
+
+	return 0, nil
 }
